9de48b694b3dc08dfe66a81a034aec0a335bb59c
= <atossell91@gmail.com>
2025-07-12 17:57:59 -0700

    Check commit

--- alina.py
+++ alina.py
@@ -1,5 +1,15 @@
+import argparse
+
 def main():
-    create_file("/home/ant/Programming/alina/templates/test.txt.al", girlType="Fat Girls")
+    args = argparse.ArgumentParser("Alina")
+    args.add_argument("command", help="The command to execute", choices=[
+        "class", "header", "interface", "namespace"
+    ])
+
+    args.add_argument("-name", help="The file name")
+    args.add_argument("-namespace", help="The namespace to use")
+
+    args.parse_args()
 
 def create_file(template_path, **kwargs):
     text = ""
@@ -10,6 +20,9 @@ def create_file(template_path, **kwargs):
         text = text.replace(f'{{{{ {key} }}}}', val)
 
     print(text)
+
+def get_current_folder():
+    pass
 
 if __name__ == '__main__':
     main()
